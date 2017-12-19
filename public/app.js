7da7fbc6c4c4e0ddc70f5715dab53801744c7953
IShoQ <pancerzp@gmail.com>
2017-12-19 18:26:18 +0100

    Ściągnięcie ui routera

--- public/app.js
+++ public/app.js
@@ -4,21 +4,27 @@ app.config(function($routeProvider) {
     // $locationProvider.hashPrefix('');
 
     $routeProvider.when('/', {
+            controller: 'IntBuildController',
             templateUrl: 'views/home.html'
         })
         .when('/login', {
+            controller: 'IntBuildController',
             templateUrl: 'views/login.html'
         })
         .when('/register', {
+            controller: 'IntBuildController',
             templateUrl: 'views/register.html'
         })
         .when('/home', {
+            controller: 'IntBuildController',
             templateUrl: 'views/home.html'
         })
         .when('/components', {
+            controller: 'IntBuildController',
             templateUrl: 'views/home.html'
         })
         .when('/chart', {
+            controller: 'IntBuildController',
             templateUrl: 'views/home.html'
         })
         .otherwise({
